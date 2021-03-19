f74385b505f61b6af8b1ca1dbc8a565898c3eb08
Hennadii20 <ltava2021@gmail.com>
2021-03-19 21:36:18 +0200

    full task

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-const url = `http://www.omdbapi.com/?s=${title}&y=${year}&apikey=1cbe0d07`;
+const output = document.querySelector('.output');
+
+function createMovie(name, linkToPoster, year){
+    const title = document.createElement('h2');
+    const titleText = document.createTextNode(name);
+    
+    title.appendChild(titleText);
+
+    const poster = document.createElement('img');
+    poster.src = linkToPoster;
+    poster.alt = name;
+
+    const yearText = document.createTextNode(year);
+    const yearP = document.createElement('p');
+    yearP.appendChild(yearText);
+
+    const HR = document.createElement('hr');
+
+    const movieSection =  document.createElement('section');
+
+    movieSection.appendChild(title);
+    movieSection.appendChild(poster);
+    movieSection.appendChild(yearP);
+    movieSection.appendChild(HR);
+
+    return movieSection;
+}
+
   
 function sendDataToAPI(title, year){
+    const url = `http://www.omdbapi.com/?s=${title}&y=${year}&apikey=1cbe0d07`;
+    
     fetch(url)
     .then((response) => {
         return response.json();
     })
-    .then((result) => console.log(result))
-
-    .catch((error) => {
-        console.log(error);
-    })
- }
+    .then((result) => {
+        result.Search.forEach((movie) => {
+            const movieLayOut = createMovie(movie.Title, movie.Poster, movie.Year);
+            output.appendChild(movieLayOut);
+        })
+    });
+    
+    // .catch((error) => {
+    //     console.log(error);
+}
 
 $('.user-form').on('submit', (event) => {
     event.preventDefault();
@@ -19,3 +52,4 @@ $('.user-form').on('submit', (event) => {
     sendDataToAPI(title, year);
     console.log(title, year); 
 });
+
